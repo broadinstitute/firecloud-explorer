ed74a02268bc9944b9606890ba511878178af0ea
Diego Gil <diego.a.gil@gmail.com>
2017-11-15 11:35:51 -0300

    fixing conf issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,15 @@
 export const environment = {
   production: true,
+  testing: false,
+  GOOGLE_URL: 'https://www.googleapis.com/',
+  // google authorization values
   CLIENT_ID: '454950129951-4nd5ah1v74efvi4619m5f075q4dp1puq.apps.googleusercontent.com',
   CLIENT_SECRET: '7lWnz5qgiiPPkNr18kS1-DdF',
-  BUCKET_NAME: 'bucket-privado'
+  REDIRECT_URI: 'http://localhost',
+  BUCKET_NAME: 'bucket-privado',
+  AUTHORIZATION_URL: 'https://accounts.google.com/o/oauth2/v2/auth',
+  TOKEN_URL: 'https://www.googleapis.com/oauth2/v4/token',
+  GOOGLE_PROFILE_URL: 'https://www.googleapis.com/userinfo/v2/me',
+  FIRECLOUD_API: 'https://firecloud-orchestration.dsde-alpha.broadinstitute.org/'
 };
 
