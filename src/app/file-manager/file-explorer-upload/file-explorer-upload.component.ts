6613979904445d7e6321763b9f5bab8a52565abd
vernicavicario <vernicavicario@yahoo.com.ar>
2018-03-29 14:35:55 -0300

    fix spec

--- src/app/file-manager/file-explorer-upload/file-explorer-upload.component.ts
+++ src/app/file-manager/file-explorer-upload/file-explorer-upload.component.ts
@@ -215,7 +215,7 @@ export class FileExplorerUploadComponent implements OnInit {
           .forEach(file => {
             this.dataFile = new Item(UUID.UUID(), file.data.name, file.data.updated, file.data.updated, file.data.size,
               '', file.data.path, result.directory, Type.UPLOAD, ItemStatus.PENDING, '',
-              '', '', result.preserveStructure, false, '', '', '', true);
+              '', '', result.preserveStructure, false, '', file.data.name, '', true);
 
             this.uploadFiles.push(this.dataFile);
             this.store.dispatch(new Transferables.AddItem(this.dataFile));
