856dbeb313dc239ce497eeb16ea1571618e63bcd
Walter <wloforte@broadinstitute.org>
2018-04-17 12:22:32 -0300

    Fix build scripts

--- scripts/linux-creator.sh
+++ scripts/linux-creator.sh
@@ -10,4 +10,4 @@ mkdir -p app/linux/installers
 ./node_modules/.bin/electron-packager . --overwrite --asar --platform=linux --arch=x64 --icon=src/assets/icons/png/1024x1024.png --prune=true --out=app/linux/builds --app-copyright='Copyright © 2018 Broad Institute All Rights Reserved' --app-version='0.1.0'
 
 # create .deb installer
-./node_modules/.bin/electron-installer-debian --src 'app/linux/builds/FireCloud Explorer-linux-x64/' --arch amd64 --config installers/linux/debian.json
+./node_modules/.bin/electron-installer-debian --src 'app/linux/builds/FireCloud DataShuttle-linux-x64/' --arch amd64 --config installers/linux/debian.json
