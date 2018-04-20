48fbc154dbb60c71bdab0332d85a208e853aae08
Diego Gil <dgil@belatrixsf.com>
2018-04-20 17:27:58 -0300

    fixing PR comments

--- src/app/file-manager/models/type.ts
+++ src/app/file-manager/models/type.ts
@@ -1,11 +1,17 @@
 export class Type {
-     static readonly DOWNLOAD = 'Download';
-     static readonly UPLOAD = 'Upload';
-     static readonly FILE = 'File';
-     static readonly FOLDER = 'Folder';
-     static readonly EXPORT_S3 = 'Export S3';
-     static readonly EXPORT_GCP = 'Export GCP';
+    /**
+     *  following values are deprecated and will be removed in a next version
+     */
+    static readonly DOWNLOAD = 'Download';
+    static readonly UPLOAD = 'Upload';
+    static readonly FILE = 'File';
+    static readonly FOLDER = 'Folder';
+    static readonly EXPORT_S3 = 'Export S3';
+    static readonly EXPORT_GCP = 'Export GCP';
 
+    /**
+     * currently, only following numeric types should be used 
+     */
     static readonly IDOWNLOAD = 0;
     static readonly IUPLOAD = 1;
     static readonly IFILE = 2;
