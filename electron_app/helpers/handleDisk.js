b7e3a34962f896bad839d586874191c56d669ed0
vvicario <vernicavicario@yahoo.com.ar>
2018-04-30 10:59:21 -0300

    write permission on windows (#71)

--- electron_app/helpers/handleDisk.js
+++ electron_app/helpers/handleDisk.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+var tmp = require('tmp');
 const mkdirp = require('mkdirp');
 const diskspace = require('diskspace');
 const electron = require('electron');
@@ -30,26 +31,37 @@ const handleDiskSpace = (destination, totalFileSize) => {
         }
         try {
             diskspace.check(drive, function (dskErr, diskSpace) {
-                canWrite(destination, (wrtErr, isWritable) => {
-                    if (diskSpace.free < totalFileSize) {
-                        conditions.error = true;
-                        conditions.errorMessage = 'Sorry, there is not enough space on disk.';
-                    } else if (!isWritable) {
-                        conditions.error = true;
-                        conditions.errorMessage = 'Sorry, you don\'t have the proper permissions to access that location.';
-                    }
+                if (diskSpace.free < totalFileSize) {
+                    conditions.error = true;
+                    conditions.errorMessage = 'Sorry, there is not enough space on disk.';
                     res(conditions);
-                });
-            })
+                } else {
+                    canWrite(destination, (wrtErr, isWritable) => {
+                        if (!isWritable) {
+                            conditions.error = true;
+                            conditions.errorMessage = 'Sorry, you don\'t have the proper permissions to access that location.';
+                        }                     
+                        res(conditions);
+                    });
+                }
+            });
         } catch (err) {
             rej(err);
         }
     });
 };
 const canWrite = (path, callback) => {
-    fs.access(path, fs.W_OK, function (err) {
-        callback(null, !err);
-    });
+    if (process.platform === 'win32') {
+        let temp = path + '/tmp-XXXXXX';
+        tmp.dir({ template: temp }, function _tempDirCreated(err, path, cleanupCallback) {
+            callback(null, !err);
+            if (!err) cleanupCallback();
+        });
+    } else {
+        fs.access(path, fs.W_OK, function (err) {
+            callback(null, !err);
+        });
+    }
 };
 
 module.exports = { handleFolder, handleDiskSpace, fileAlreadyExists };
