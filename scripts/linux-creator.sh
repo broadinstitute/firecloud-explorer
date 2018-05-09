8060d5658638df4b13dc2b130b8de8cdf5799b46
Diego Gil <diego.gil@maipucinos.com.ar>
2018-05-09 17:54:50 -0300

    0.1.1 release (#75)

--- scripts/linux-creator.sh
+++ scripts/linux-creator.sh
@@ -7,7 +7,7 @@ rm -rf app/linux/*
 mkdir -p app/linux/installers
 
 # package the application as .app
-./node_modules/.bin/electron-packager . --overwrite --asar --platform=linux --arch=x64 --icon=src/assets/icons/png/1024x1024.png --prune=true --out=app/linux/builds --app-copyright='Copyright © 2018 Broad Institute All Rights Reserved' --app-version='0.1.0'
+./node_modules/.bin/electron-packager . --overwrite --asar --platform=linux --arch=x64 --icon=src/assets/icons/png/1024x1024.png --prune=true --out=app/linux/builds --app-copyright='Copyright © 2018 Broad Institute All Rights Reserved' --app-version='0.1.1'
 
 # create .deb installer
 ./node_modules/.bin/electron-installer-debian --src 'app/linux/builds/FireCloud DataShuttle-linux-x64/' --arch amd64 --config installers/linux/debian.json
