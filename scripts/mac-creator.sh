fd769effb196ae92abf3fdd9a21cb9110cba4b82
Diego Gil <dgil@belatrixsf.com>
2018-05-09 17:13:59 -0300

    0.1.1 release

--- scripts/mac-creator.sh
+++ scripts/mac-creator.sh
@@ -7,7 +7,7 @@ rm -rf app/mac/*
 mkdir -p app/mac/installers
 
 # package the application as .app
-./node_modules/.bin/electron-packager . --overwrite --asar --platform=darwin --arch=x64 --icon=src/assets/icons/mac/FCExplorer.icns --prune=true --out=app/mac/builds --app-bundle-id=org.broadinstitute.firecloud-explorer --app-copyright='Copyright © 2018 Broad Institute All Rights Reserved' --app-version='0.1.0' --app-category-type=public.app-category.medical
+./node_modules/.bin/electron-packager . --overwrite --asar --platform=darwin --arch=x64 --icon=src/assets/icons/mac/FCExplorer.icns --prune=true --out=app/mac/builds --app-bundle-id=org.broadinstitute.firecloud-explorer --app-copyright='Copyright © 2018 Broad Institute All Rights Reserved' --app-version='0.1.1' --app-category-type=public.app-category.medical
 
 # Sign generated .app 
 # <identity> is the hex id of imported certificate
